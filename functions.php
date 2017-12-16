a5fc732de5cce78a3cca57da038031b3b1cedaf3
dinix721 <dinikasen@gmail.com>
2017-12-17 00:55:40 +0530

    Adding and editting staff completed

--- functions.php
+++ functions.php
@@ -9,29 +9,29 @@
 $connection = mysqli_connect('localhost','root','');
 mysqli_select_db($connection,'EMS_DB')or die("Error occurred");
 
-$sql = "delimiter //
-    create function distance_result(student_id varchar(20))
+
+$sql= "Create function distance_result(student_id varchar(20))
     returns int
       begin
          declare d_result int;
          declare distance numeric(5,2);
          set d_result =0;
          select distanceToSchl into distance from apply where application_ID = student_id;
-         IF(distance <= 1) THEN
+         IF distance <= 1 THEN
             SET d_result = 60;
-         ELSE IF(distance <=3) THEN
+         ELSEIF distance <=3 THEN
             SET d_result = 48;
-         ELSE IF(distance <=5) THEN
+         ELSEIF distance <=5 THEN
             SET d_result = 36;
          ELSE
             SET d_result = 24; 
          END IF;
          RETURN d_result;
-      end//"
+      end";
+$retval = mysqli_query( $connection, $sql );
+if(! $retval ) {
+    die('Could not create function: ');
+}
+echo "Function created successfully\n";
+
 ?>
-CREATE FUNCTION calcProfit(cost FLOAT, price FLOAT) RETURNS DECIMAL(9,2)
-BEGIN
-DECLARE profit DECIMAL(9,2);
-SET profit = price-cost;
-RETURN profit;
-END$$
